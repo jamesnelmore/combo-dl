069ad6bc0a955779e452f1f7d890f41cb8bc2467
James Elmore <email@jameselmore.org>
2025-09-12 00:49:16 -0400

    Delete `old` directory and move useful code out

---
+++ src/thesis/environments/__init__.py
@@ -0,0 +1,7 @@
+"""Environments for reinforcement learning experiments.
+
+This module contains Gymnasium environments for various graph optimization problems.
+Currently includes environments for inverse eigenvalue problems and graph construction.
+"""
+
+__all__ = []
