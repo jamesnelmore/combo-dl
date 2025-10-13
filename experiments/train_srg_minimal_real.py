5164b8f060b897f4dc25cce8f2c3257f22c5ba12
James Elmore <email@jameselmore.org>
2025-10-13 15:08:25 -0400

    Added device selection to minimal training script

--- experiments/train_srg_minimal_real.py
+++ experiments/train_srg_minimal_real.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 
+import torch
+
 from combo_dl.algorithms import WagnerDeepCrossEntropy
 from combo_dl.models import FFModel
 from combo_dl.problems import StronglyRegularGraphs
 
-N = 10
-K = 3
-LAMBDA = 0
-MU = 1
+N = 17
+K = 8
+LAMBDA = 3
+MU = 4
 
 ITERATIONS = 10_000
 BATCH_SIZE = 2048
@@ -15,8 +17,12 @@ LEARNING_RATE = 0.001
 ELITE_PROPORTION = 0.1
 EARLY_STOPPING_PATIENCE = ITERATIONS
 
+device = (
+    "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
+)
 
-def main():  # noqa: D103
+
+def main():
     problem = StronglyRegularGraphs(N, K, LAMBDA, MU)
     model = FFModel(N, hidden_layer_sizes=[64, 32, 16, 8, 4])
     dce = WagnerDeepCrossEntropy(
@@ -29,6 +35,7 @@ def main():  # noqa: D103
         early_stopping_patience=EARLY_STOPPING_PATIENCE,
         wandb_mode="online",
         checkpoint_dir=Path("checkpoints"),
+        device=device,
     )
     dce.optimize()
 
