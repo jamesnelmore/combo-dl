01df9910ddfd79e3d7cb7ebd04ce50806699a7b4
James Elmore <email@jameselmore.org>
2026-07-18 13:52:37 -0400

    Delete extra scripts

--- scripts/setup_jupyter_git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip install nb-clean
-
-git config --local filter.nb-clean.clean "nb-clean clean"
-git config --local filter.nb-clean.required true
-
-echo "Jupyter notebook Git configuration complete!" 
